047b9f379df9252f0f4d47486e51c741942f1603
Colin Howells <colin@colinhowells.com>
2024-05-30 11:49:29 -0700

    less insistent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,14 +11,6 @@
 <style>
 	h2 span {
 		margin-left: 0.25rem;
-		/* display: inline-flex;
-		flex: 0 1 auto; */
-		/* vertical-align: middle;
-		margin: -3px 0.5rem 0 0; */
-		/* border-radius: 0.15rem; */
-		/* background: var(--color-cool-400); */
-		/* padding: 0.25rem 0.3rem 0.25rem 0.5rem; */
-		/* color: white; */
 		color: var(--color-cool-600);
 		font-weight: 500;
 		font-size: 0.75rem;
