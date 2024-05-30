c7620006b91ec98adea425fe20b904bb22e1c8c1
Colin Howells <colin@colinhowells.com>
2024-05-30 11:49:10 -0700

    less insistent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,23 +5,25 @@
 </script>
 
 {#each data.articles as article}
-	<h2><span>{article.type}</span> <a href="/{article.slug}">{article.title}</a></h2>
+	<h2><a href="/{article.slug}">{article.title}</a> <span>{article.type}</span></h2>
 {/each}
 
 <style>
 	h2 span {
-		display: inline-flex;
-		flex: 0 1 auto;
-		vertical-align: middle;
-		margin: -3px 0.5rem 0 0;
-		border-radius: 0.15rem;
-		background: var(--color-cool-400);
-		padding: 0.25rem 0.3rem 0.25rem 0.5rem;
-		color: white;
-		font-weight: 800;
-		font-size: 0.5rem;
+		margin-left: 0.25rem;
+		/* display: inline-flex;
+		flex: 0 1 auto; */
+		/* vertical-align: middle;
+		margin: -3px 0.5rem 0 0; */
+		/* border-radius: 0.15rem; */
+		/* background: var(--color-cool-400); */
+		/* padding: 0.25rem 0.3rem 0.25rem 0.5rem; */
+		/* color: white; */
+		color: var(--color-cool-600);
+		font-weight: 500;
+		font-size: 0.75rem;
 		font-family: var(--font-sans);
-		letter-spacing: 2px;
+		letter-spacing: 1px;
 		text-transform: uppercase;
 	}
 </style>
