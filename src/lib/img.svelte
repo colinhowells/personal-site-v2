3c1bb07be2acd860e17394085045e462ffc69b88
Colin Howells <colin@colinhowells.com>
2024-09-06 16:32:18 -0700

    Vercel img srcset

--- src/lib/img.svelte
+++ src/lib/img.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
+
 	export let src: string;
 	export let alt: string;
 	export let title: string;
+
+	/** @see https://vercel.com/docs/frameworks/sveltekit#image-optimization */
+	function optimize(src: string, widths = [330, 430, 600], quality = 90) {
+		if (dev) return src;
+
+		return widths
+			.slice()
+			.sort((a, b) => a - b)
+			.map((width, i) => {
+				const url = `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`;
+				const descriptor = i < widths.length - 1 ? ` ${width}w` : '';
+				return url + descriptor;
+			})
+			.join(', ');
+	}
 </script>
 
 <figure>
-	<img {src} loading="lazy" {alt} {title} />
+	<img srcset={optimize(src)} loading="lazy" {alt} {title} />
 	{#if title || alt}
 		<figcaption>{title ?? alt}</figcaption>
 	{/if}
