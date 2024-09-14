14779499f456fd6b72bef6df0e3e2ff68208bd8c
Colin Howells <colin@colinhowells.com>
2024-09-14 14:39:55 -0700

    leaving img srces alone for now

--- src/lib/img.svelte
+++ src/lib/img.svelte
@@ -5,24 +5,27 @@
 	export let alt: string;
 	export let title: string;
 
-	/** @see https://vercel.com/docs/frameworks/sveltekit#image-optimization */
-	function optimize(src: string, widths = [330, 430, 600], quality = 90) {
-		if (dev) return src;
+	// /**
+	//  * @see https://vercel.com/docs/frameworks/sveltekit#image-optimization
+	//  * @example srcset={optimize(src)}
+	//  * */
+	// function optimize(src: string, widths = [330, 430, 600], quality = 90) {
+	// 	if (dev) return src;
 
-		return widths
-			.slice()
-			.sort((a, b) => a - b)
-			.map((width, i) => {
-				const url = `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`;
-				const descriptor = i < widths.length - 1 ? ` ${width}w` : '';
-				return url + descriptor;
-			})
-			.join(', ');
-	}
+	// 	return widths
+	// 		.slice()
+	// 		.sort((a, b) => a - b)
+	// 		.map((width, i) => {
+	// 			const url = `/_vercel/image?url=${encodeURIComponent(src)}&w=${width}&q=${quality}`;
+	// 			const descriptor = i < widths.length - 1 ? ` ${width}w` : '';
+	// 			return url + descriptor;
+	// 		})
+	// 		.join(', ');
+	// }
 </script>
 
 <figure>
-	<img srcset={optimize(src)} loading="lazy" {alt} {title} />
+	<img {src} loading="lazy" {alt} {title} />
 	{#if title}
 		<figcaption>{title}</figcaption>
 	{/if}
