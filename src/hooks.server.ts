479a60f6f32632b20086e12650e436c4d7cdb171
Colin Howells <colin@colinhowells.com>
2025-08-21 17:15:09 -0700

    CSP revision

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,16 +7,16 @@ export const handle: Handle = async ({ event, resolve }) => {
 			return type === 'font' || type === 'js' || type === 'css';
 		}
 	});
-	response.headers.set(
-		'Content-Security-Policy',
-		"default-src 'self'; frame-ancestors 'none'; form-action 'self';"
-	);
+
+	response.headers.set('Content-Security-Policy', "frame-ancestors 'none'; form-action 'self';");
 	response.headers.set('Permissions-Policy', 'interest-cohort=()');
 	response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
 	response.headers.set('X-Content-Type-Options', 'nosniff');
 	response.headers.set('X-Frame-Options', 'DENY');
+
 	if (PUBLIC_ENVIRONMENT !== 'production') {
 		response.headers.set('X-Robots-Tag', 'noindex');
 	}
+
 	return response;
 };
