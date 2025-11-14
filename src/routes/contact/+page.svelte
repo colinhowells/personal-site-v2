7507844a306369598f0f71a6fc770657bb57a6f0
Colin Howells <colin@colinhowells.com>
2025-11-13 17:26:45 -0800

    rm old form, config tweaks

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -38,68 +38,6 @@
 	{/each}
 </form>
 
-<!-- <form method="POST" aria-label="Contact form" use:enhance>
-	<label for="name">
-		Name
-		<input
-			value={name}
-			type="text"
-			name="name"
-			id="name"
-			required
-			autocomplete="name"
-			aria-invalid={error_field === 'name'}
-			aria-describedby={error_field === 'name' ? 'name-error' : undefined}
-		/>
-	</label>
-	{#if error_field === 'name'}
-		<small id="name-error" aria-live="polite">{error_message}</small>
-	{/if}
-
-	<label for="email">
-		Email
-		<input
-			value={email}
-			type="email"
-			name="email"
-			id="email"
-			required
-			autocomplete="email"
-			aria-invalid={error_field === 'email'}
-			aria-describedby={error_field === 'email' ? 'email-error' : undefined}
-		/>
-	</label>
-	{#if error_field === 'email'}
-		<small id="email-error" aria-live="polite">{error_message}</small>
-	{/if}
-
-	<label for="message">
-		Your message
-		<textarea
-			name="message"
-			id="message"
-			cols="30"
-			rows="10"
-			required
-			aria-invalid={error_field === 'message'}
-			aria-describedby={error_field === 'message' ? 'message-error' : undefined}>{message}</textarea
-		>
-	</label>
-	{#if error_field === 'message'}
-		<small id="message-error" aria-live="polite">{error_message}</small>
-	{/if}
-
-	{#if error_field === 'submit'}
-		<small id="submit-error" aria-live="polite">{error_message}</small>
-	{/if}
-
-	{#if form?.success}
-		<p>Thanks for your message!</p>
-	{:else}
-		<button>Submit</button>
-	{/if}
-</form> -->
-
 <style>
 	form {
 		display: flex;
