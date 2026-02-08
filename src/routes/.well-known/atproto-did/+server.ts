6c9aadf2884c22ebd38f10ca4bb93034520d239b
Colin Howells <colin@colinhowells.com>
2026-02-07 21:25:23 -0800

    we needed a header explicitly set

--- src/routes/.well-known/atproto-did/+server.ts
+++ src/routes/.well-known/atproto-did/+server.ts
@@ -2,5 +2,5 @@ import { text } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = () => {
-	return text('did:plc:5rcvs5qzp6fhigken546ymtl');
+	return text('did:plc:5rcvs5qzp6fhigken546ymtl', { headers: { 'Content-Type': 'text/plain' } });
 };
