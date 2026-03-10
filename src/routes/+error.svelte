754202146ce0014f4795623cd54b608279aa73d8
Colin Howells <colin@colinhowells.com>
2026-03-09 18:45:19 -0700

    isError helper, move to Mise via 'devEngines'

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import { isError } from '$lib/helpers';
 
 	let { error } = $props();
 </script>
 
 <article>
 	<h2 class="title">Whoops! {page.status}</h2>
-	<p>{error instanceof Error ? error?.message : String(error)}</p>
+	<p>{isError(error) ? error.message : String(error)}</p>
 </article>
